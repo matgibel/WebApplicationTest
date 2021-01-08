7f4df7f9fd981232e936f81cd58984d6b202bbb7
Mathew Gibel <matgibel@gmail.com>
2021-01-08 17:24:22 -0500

    first commit

---
+++ WebApplicationTest/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApplicationTest.Models;
+
+namespace WebApplicationTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+        [HttpGet]
+        public ActionResult CreateUser()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateUser(User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(user);
+            }
+            
+            return Redirect("Index");
+        }
+    }
+}
---
+++ WebApplicationTest/Models/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace WebApplicationTest.Models
+{
+    public class User
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+
+        
+        [EmailAddress]
+        public EmailAddressAttribute Email { get; set; }
+        
+        
+        [Required]
+        [PasswordPropertyText]
+        public string Password { get; set; }
+        [Required]
+        [Compare("Password", ErrorMessage = "Confirm password")]
+        public string ConfirmPassword { get; set; }
+
+        
+        public DateTime CreationTime { get; set; }
+        
+        public DateTime LogginTime { get; set; }
+    }
+}
